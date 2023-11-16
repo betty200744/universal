369c69a2c23cabdebcea58db182dcbc86d347125
betty200744 <htzhao200744@gmail.com>
2023-11-16 11:25:23 +0800

    v

--- src/store/actions/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-import { SHOP_INFO, CHANGE_SHOP_INFO, CHANGE_PHONE_LOGIN, CHANHE_COUPON_MODAL } from '../types';
-
-export const changeShopInfo = createAction(CHANGE_SHOP_INFO, (target: string, value: string) => ({ target, value }));
-
-export const changePhoneLogin = createAction(CHANGE_PHONE_LOGIN, (value: boolean) => ({ show: value }));
-
-export const changeCouponModal = createAction(CHANHE_COUPON_MODAL, (value: boolean) => ({ show: value }));
