efc82b255bccfbe3f9f95d8340826cdfb31901d7
betty200744 <htzhao200744@gmail.com>
2023-11-22 14:07:11 +0800

    v

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,4 +1,13 @@
 import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import { SHOP_INFO } from '../types';
+
+const shopInfo = handleActions({
+  [SHOP_INFO](state, action) {
+    return state;
+  },
+}, {});
 
 export default combineReducers({
+  shopInfo,
 });
